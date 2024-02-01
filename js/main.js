6123b6dea5d69872485653b93b8d9874a6a05afb
Євгеній <catrockout@icloud.com>
2024-02-01 14:51:23 +0200

    Commit v2.0 - Bug fixes

--- js/main.js
+++ js/main.js
@@ -1,12 +1,39 @@
 function initToDoList() {
-    const inputNote = document.querySelector('.input-creation');
     const submitNote = document.querySelector('.submit-creation');
+
+    if (!submitNote) {
+        return;
+    }
+
+    const submitButtonText = document.querySelector('.submit-creation__save');
+    const inputNote = document.querySelector('.input-creation');
     const todoList = document.querySelector('.todo-list');
     const deleteAllNote = document.querySelector('.clean-items');
+    // To save a link to the edited note:
+    let editingNote = null;
+
+    // Function to update the old cookie when editing a note:
+    function updateCookie(oldName, newName) {
+        const oldCookieString = `${encodeURIComponent(oldName)}=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/`;
+        document.cookie = oldCookieString;
+
+        const date = new Date();
+        date.setHours(date.getHours() + 2);
+
+        const newCookieString = `${encodeURIComponent(newName)}=; expires=${date}; path=/`;
+        document.cookie = newCookieString;
+    }
+
+    // Function to delete cookies:
+    function deleteCookie(cookieName) {
+        document.cookie = `${encodeURIComponent(cookieName)}=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/`;
+    }
 
     // Create and Add a new note:
     submitNote.addEventListener('click', () => {
-        if (inputNote.value !== '' && submitNote.textContent === 'Save') {
+        const currentTextInSubBtn = submitButtonText.getAttribute('data-text');
+
+        if (inputNote.value !== '' && currentTextInSubBtn === 'Save' && !editingNote) {
             const newItem = document.createElement('li');
             newItem.classList.add('todo-list__item');
             newItem.innerHTML = `
@@ -19,65 +46,70 @@ function initToDoList() {
 
             todoList.appendChild(newItem);
             inputNote.value = '';
+
+            // Add a cookie only if the note has a name:
+            if (inputNote.value.trim() !== '') {
+                const date = new Date();
+                date.setHours(date.getHours() + 2);
+    
+                const cookieString = `${encodeURIComponent(inputNote.value)}=; expires=${date}; path=/`;
+                document.cookie = cookieString;
+            }
+
+        // Editing a note - transfer text from input to a note:
+        } else if (currentTextInSubBtn === 'Edit' && editingNote) {
+            const saveNote = editingNote.querySelector('.save');
+            const deleteNote = editingNote.querySelector('.delete');
+            const oldCookieName = editingNote.querySelector('.item-title').textContent;
+            const newCookieName = inputNote.value;
+            editingNote.querySelector('.item-title').textContent = newCookieName;
+
+            submitButtonText.setAttribute('data-text', 'Save');
+            submitNote.classList.add('submit-creation');
+            submitNote.classList.remove('submit-creation-red');
+            saveNote.classList.remove('save-success');
+            deleteNote.classList.remove('delete-red');
+            deleteAllNote.classList.remove('clean-items-red');
+
+            editingNote = null;
+            inputNote.value = '';
+
+            // Find and update the corresponding cookie:
+            saveNote.addEventListener('click', () => {
+                if (oldCookieName) {
+                    updateCookie(oldCookieName, newCookieName);
+                }
+            });
         }
     });
-
-    // Function to update the old cookie when editing a note:
-    function updateCookie(oldName, newValue, newName) {
-        const oldCookieString = `${encodeURIComponent(oldName)}=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/`;
-        document.cookie = oldCookieString;
-
-        let date = new Date();
-        date.setHours(date.getHours() + 2);
-
-        const newCookieString = `${encodeURIComponent(newName)}=${encodeURIComponent(newValue)}; expires=${date}; path=/`;
-        document.cookie = newCookieString;
-    }
-
-    // Function to delete cookies:
-    function deleteCookie(cookieName) {
-        document.cookie = `${encodeURIComponent(cookieName)}=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/`;
-    }
 
     // Editing a note:
     todoList.addEventListener('click', (event) => {
         const clickedElement = event.target;
         const parentListItem = clickedElement.closest('.todo-list__item');
+
         if (!parentListItem) {
             return;
         }
+
         const saveNote = parentListItem.querySelector('.save');
         const deleteNote = parentListItem.querySelector('.delete');
         const cookieName = parentListItem.querySelector('.item-title').textContent;
-        const cookieValue = parentListItem;
+        const cookieValue = parentListItem.querySelector('.item-title').textContent;
         
         // moving text to input when clicking on the note:
         if (clickedElement.classList.contains('item-title')) {
             inputNote.value = clickedElement.textContent;
-            submitNote.classList.add('submit-creation-red');
-            submitNote.innerHTML = 'Edit';
-        }
+            editingNote = parentListItem;
 
-        // editing the content of the text from the input to the note:
-        if (submitNote.textContent === 'Edit') {
-            submitNote.addEventListener('click', () => {
-                clickedElement.innerHTML = inputNote.value;
-                submitNote.classList.remove('submit-creation-red');
-                submitNote.innerHTML = 'Save';
-                saveNote.classList.remove('save-success');
-                deleteNote.classList.remove('delete-red');
-                inputNote.value = '';
-    
-                // Find and update the corresponding cookie:
-                const newCookieName = inputNote.value;
-                const newCookieValue = inputNote.value;
-                updateCookie(cookieName, newCookieValue, newCookieName);
-            });
+            submitButtonText.setAttribute('data-text', 'Edit');
+            submitNote.classList.add('submit-creation-red');
+            submitNote.classList.remove('submit-creation');
         }
 
         // Creation cookie:
         if (clickedElement === saveNote) {
-            let date = new Date();
+            const date = new Date();
             date.setHours(date.getHours() + 2);
 
             const cookieString = `${encodeURIComponent(cookieName)}=${encodeURIComponent(cookieValue)}; expires=${date}; path=/`;
@@ -85,28 +117,25 @@ function initToDoList() {
     
             saveNote.classList.add('save-success');
             deleteNote.classList.add('delete-red');
+            deleteAllNote.classList.add('clean-items-red');
         }
         
         // Deleting cookie and note:
-        if (clickedElement === deleteNote) {
+        if (!deleteNote.classList.contains('delete-red')) {
+            return;
+        } else if (clickedElement === deleteNote) {
             const cookieString = `${encodeURIComponent(cookieName)}=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/`;
             document.cookie = cookieString;
             deleteCookie(cookieName);
 
             parentListItem.remove();
+            submitNote.classList.add('submit-creation');
+            submitNote.classList.remove('submit-creation-red');
             saveNote.classList.remove('save-success');
             deleteNote.classList.remove('delete-red');
+            deleteAllNote.classList.remove('clean-items-red');
         }
     });
-
-    function deleteAllCookies() {
-        const cookies = document.cookie.split(";");
-    
-        for (const cookie of cookies) {
-            const cookieName = cookie.trim().split("=")[0];
-            document.cookie = `${encodeURIComponent(cookieName)}=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/`;
-        }
-    }
 
     // Deleting all cookies and notes:
     deleteAllNote.addEventListener('click', () => {
@@ -120,6 +149,8 @@ function initToDoList() {
 
         // Clearing the TODO list:
         todoList.innerHTML = '';
+
+        deleteAllNote.classList.remove('clean-items-red');
     });
 }
 
@@ -128,25 +159,30 @@ document.addEventListener('DOMContentLoaded', () => {
     if (!document.cookie) {
         return;
     }
+    
     const allCookies = document.cookie.split('; ');
     const todoList = document.querySelector('.todo-list');
 
     allCookies.forEach((cookie) => {
-        const [cookieName, cookieValue] = cookie.split('=');
-        const newItem = document.createElement('li');
+        const [cookieName] = cookie.split('=');
+        
+        if (cookieName.trim() !== '') {
+            const newItem = document.createElement('li');
 
-        newItem.classList.add('todo-list__item');
-        newItem.innerHTML = `
-            <p class="item-title">${decodeURIComponent(cookieName)}</p>
-            <div class="todo-list_buttons">
-                <button class="save"></button>
-                <button class="delete"></button>
-            </div>
-        `;
-
-        todoList.appendChild(newItem);
-        newItem.querySelector('.save').classList.add('save-success');
-        newItem.querySelector('.delete').classList.add('delete-red');
+            newItem.classList.add('todo-list__item');
+            newItem.innerHTML = `
+                <p class="item-title">${decodeURIComponent(cookieName)}</p>
+                <div class="todo-list_buttons">
+                    <button class="save"></button>
+                    <button class="delete"></button>
+                </div>
+            `;
+    
+            todoList.appendChild(newItem);
+            newItem.querySelector('.save').classList.add('save-success');
+            newItem.querySelector('.delete').classList.add('delete-red');
+            document.querySelector('.clean-items').classList.add('clean-items-red');
+        }
     });
 });
 
